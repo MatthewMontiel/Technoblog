4a666afac30a26d4f670cb0a0c1acae7c326471a
matthewmontiel <xoptimuszerox@gmail.com>
2023-06-07 14:47:16 -0500

    added index in controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeroutes");
-const dashboardRoutes 
+const dashboardRoutes = require("./dashboard-routes");
 // establish routes ... does this order matter
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
+router.use("/dashboard", dashboardRoutes);
 
 module.exports = router;
