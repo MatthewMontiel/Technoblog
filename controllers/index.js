bd5e71904a52418cee04292bbd0468af5af67ad9
matthewmontiel <xoptimuszerox@gmail.com>
2023-06-17 16:43:18 -0500

    added home routes js

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
-const homeRoutes = require("./homeroutes");
-const dashboardRoutes = require("./dashboard-routes");
-// establish routes ... does this order matter
+const homeRoutes = require("./homeroutes.js");
+const dashboardRoutes = require("./dashboard-routes.js");
+
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 router.use("/dashboard", dashboardRoutes);
